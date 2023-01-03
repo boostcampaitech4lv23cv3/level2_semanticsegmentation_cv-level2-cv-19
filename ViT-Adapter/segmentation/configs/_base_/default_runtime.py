db43a91d4edcc972f6e7429dc5177ed7259cecbe
JunghoYoo <jh10.yoo@gmail.com>
2023-01-03 03:38:05 +0000

    feat: add wandb

--- ViT-Adapter/segmentation/configs/_base_/default_runtime.py
+++ ViT-Adapter/segmentation/configs/_base_/default_runtime.py
@@ -4,11 +4,17 @@ log_config = dict(
     hooks=[
         dict(type='TextLoggerHook', by_epoch=False),
         # dict(type='TensorboardLoggerHook')
+        dict(type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='semantic-segmentation',
+                name='ViT-Adapter+Mask2Former'),
+        )
+        
     ])
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 load_from = None
 resume_from = None
-workflow = [('train', 1)]
+workflow = [('train', 1), ('val', 1)]
 cudnn_benchmark = True
